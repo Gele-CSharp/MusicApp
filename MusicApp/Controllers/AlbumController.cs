084dfd8b5783aafc078f403fd50d6926e7ada95a
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-04 17:31:37 +0200

    Album controller created

---
+++ MusicApp/Controllers/AlbumController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicApp.Controllers
+{
+    public class AlbumController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
