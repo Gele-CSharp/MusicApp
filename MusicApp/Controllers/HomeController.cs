0373945455f8b626e28bbc0621e3802d48898ce4
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-04 17:44:10 +0200

    HomeController inherits BaseController

--- MusicApp/Controllers/HomeController.cs
+++ MusicApp/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MusicApp.Models;
 using System.Diagnostics;
 
 namespace MusicApp.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -13,16 +14,19 @@ namespace MusicApp.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
