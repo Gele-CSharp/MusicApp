33a83fda2edf511f6a2cac917caaa2bc58b2546a
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-04 11:57:32 +0200

    Add project files.

---
+++ MusicApp.Infrastructure/Data/MusicAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicApp.Data
+{
+    public class MusicAppDbContext : IdentityDbContext
+    {
+        public MusicAppDbContext(DbContextOptions<MusicAppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
