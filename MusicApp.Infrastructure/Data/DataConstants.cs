c90a1660a51bd92e5640f97c53873d41ab7b1d50
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-04 13:50:03 +0200

    Added new constsnts

--- MusicApp.Infrastructure/Data/DataConstants.cs
+++ MusicApp.Infrastructure/Data/DataConstants.cs
@@ -1,41 +1,46 @@
-﻿namespace MusicApp.Infrastructure.Data
+﻿namespace MusicApp.Infrastructure.Data;
+
+public static class DataConstants
 {
-    public static class DataConstants
+    public static class Album
     {
-        public static class Album
-        {
-            public const int AlbumTitleMinLength = 1;
-            public const int AlbumTitleMaxLength = 50;
+        public const int AlbumTitleMinLength = 1;
+        public const int AlbumTitleMaxLength = 50;
 
-            public const int ArtistNameMinLength = 1;
-            public const int ArtistNameMaxLength = 50;
-        }
+        public const int ArtistNameMinLength = 1;
+        public const int ArtistNameMaxLength = 50;
+    }
 
-        public static class Comment
-        {
-            public const int AuthorNameMinLength = 2;
-            public const int AuthorNameMaxLength = 50;
+    public static class Comment
+    {
+        public const int AuthorNameMinLength = 2;
+        public const int AuthorNameMaxLength = 50;
 
-            public const int ContentMinLength = 5;
-            public const int ContentMaxLength = 50;
-        }
+        public const int ContentMinLength = 5;
+        public const int ContentMaxLength = 50;
+    }
 
-        public static class User
-        {
-            public const int UserFirstNameMinLength = 2;
-            public const int UserFirstNameMaxLength = 50;
+    public static class User
+    {
+        public const int UserFirstNameMinLength = 2;
+        public const int UserFirstNameMaxLength = 50;
 
-            public const int UserLastNameMinLength = 2;
-            public const int UserLastNameMaxLength = 50;
-        }
+        public const int UserLastNameMinLength = 2;
+        public const int UserLastNameMaxLength = 50;
 
-        public static class Event
-        {
-            public const int ArtistNameMinLength = 1;
-            public const int ArtistNameMaxLength = 50;
+        public const int EmailMinLength = 10;
+        public const int EmailMaxLength = 60;
 
-            public const int LocationNameMinLength = 5;
-            public const int LocationNameMaxLength = 50;
-        }
+        public const int PasswordMinLength = 5;
+        public const int PasswordMaxLength = 20;
+    }
+
+    public static class Event
+    {
+        public const int ArtistNameMinLength = 1;
+        public const int ArtistNameMaxLength = 50;
+
+        public const int LocationNameMinLength = 5;
+        public const int LocationNameMaxLength = 50;
     }
 }
