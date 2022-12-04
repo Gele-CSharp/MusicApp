f5956aa71c1f1be7403fc3bce3629d3b603c6369
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-04 13:51:41 +0200

    Register model added

---
+++ MusicApp/Models/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static MusicApp.Infrastructure.Data.DataConstants.User;
+
+namespace MusicApp.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(EmailMaxLength, MinimumLength = EmailMinLength)]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+
+        [Required]
+        [StringLength(UserFirstNameMaxLength, MinimumLength = UserFirstNameMinLength)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(UserLastNameMaxLength, MinimumLength = UserLastNameMinLength)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = null!;
+    }
+}
