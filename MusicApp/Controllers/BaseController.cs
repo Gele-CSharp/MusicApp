1891b7eb6112cd77c75505289069dd007536deb5
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-04 17:30:14 +0200

    BaseController created

---
+++ MusicApp/Controllers/BaseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MusicApp.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        public string UserFirstName
+        {
+            get
+            {
+                string firstName = string.Empty;
+
+                if (User != null && User.HasClaim(c=> c.Type == "firstName"))
+                {
+                    firstName = User.Claims.FirstOrDefault(c => c.Type == "firstName")
+                    ?.Value ?? firstName; 
+                }
+
+                return firstName;
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            ViewBag.UserFirstName = UserFirstName;
+            base.OnActionExecuted(context);
+        }
+    }
+}
