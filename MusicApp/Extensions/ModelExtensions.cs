ff2fb1fd5df53d7324e4a78eaed01188f653ea11
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-05 14:23:11 +0200

    Model extensions made

---
+++ MusicApp/Extensions/ModelExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MusicApp.Core.Models.Album;
+using System.Text.RegularExpressions;
+
+namespace MusicApp.Extensions
+{
+    public static class ModelExtensions
+    {
+        public static string GetInformation(this IAlbumModel album)
+        => album.Title.Replace(" ", "-") + "-" + GetArtist(album.Artist);
+
+        private static string GetArtist(string artist)
+        {
+            artist = string.Join("-", artist.Split(" ").Take(3));
+            return Regex.Replace(artist, @"[^a-zA-Z0-9\-]", string.Empty);
+        }
+    }
+}
