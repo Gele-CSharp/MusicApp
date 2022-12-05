4a7a163b60dd26683bc9b7ffce2c83a4587a2d59
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-05 11:20:32 +0200

    GetLastThreeAlbums method implemented

--- MusicApp.Core/Services/AlbumService.cs
+++ MusicApp.Core/Services/AlbumService.cs
@@ -1,8 +1,37 @@
-﻿using MusicApp.Core.Contracts;
+﻿using HouseRentingSystem.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using MusicApp.Core.Contracts;
+using MusicApp.Core.Models;
+using MusicApp.Infrastructure.Data.Entities;
 
 namespace MusicApp.Core.Services
 {
     public class AlbumService : IAlbumService
     {
+        private readonly IRepository reopository;
+
+        public AlbumService(IRepository _repository)
+        {
+            reopository = _repository;
+        }
+
+        public async Task<IEnumerable<HomepageAlbumModel>> GetLastThreeAlbums()
+        {
+            var albums = await reopository
+                .AllReadonly<Album>()
+                .Where(a => a.IsActive)
+                .OrderByDescending(a => a.Id)
+                .Take(3)
+                .Select(a=> new HomepageAlbumModel() 
+                {
+                    Id = a.Id,
+                    Title = a.Title,
+                    Artist = a.Artist,
+                    ImageUrl = a.ImageUrl
+                })
+                .ToListAsync();
+
+            return albums;
+        }
     }
 }
