38e4d5b17b7666d123fdeea6ff988d835cdc26da
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-07 16:48:22 +0200

    User added

--- MusicApp.Infrastructure/Data/Entities/Comment.cs
+++ MusicApp.Infrastructure/Data/Entities/Comment.cs
@@ -22,17 +22,22 @@ namespace MusicApp.Infrastructure.Data.Entities
         [ForeignKey(nameof(User))]
         public string UserId { get; set; } = null!;
 
-        [Required]
-        [ForeignKey(nameof(Album))]
-        public int AlbumId { get; set; }
-
-
-        public virtual Album Album { get; set; } = null!;
-
         /// <summary>
         /// Author of current comment
         /// </summary>
         public User User { get; set; } = null!;
+
+        /// <summary>
+        /// Album identifier 
+        /// </summary>
+        [Required]
+        [ForeignKey(nameof(Album))]
+        public int AlbumId { get; set; }
+
+        /// <summary>
+        /// Music album 
+        /// </summary>
+        public virtual Album Album { get; set; } = null!;
 
         /// <summary>
         /// Comment content
