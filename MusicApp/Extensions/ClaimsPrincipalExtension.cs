4d5707453ba1bc739a8caf53827bccb2e90d6737
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-07 16:39:03 +0200

    ClaimsPrincipalExtension added

---
+++ MusicApp/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ClaimsPrincipalExtension
+{
+    public static string Id(this ClaimsPrincipal user)
+    {
+        return user.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
+
