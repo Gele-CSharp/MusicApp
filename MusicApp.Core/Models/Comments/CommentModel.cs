b45c349ec476f6f714a8075577e711f5d07326e0
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-07 16:49:00 +0200

    CommentModel created

---
+++ MusicApp.Core/Models/Comments/CommentModel.cs
@@ -0,0 +1,15 @@
+﻿using MusicApp.Infrastructure.Data.Entities;
+
+namespace MusicApp.Core.Models.Comments
+{
+    public class CommentModel
+    {
+        //public string? Content { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public Comment? Comment { get; set; }
+
+        public ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
+    }
+}
