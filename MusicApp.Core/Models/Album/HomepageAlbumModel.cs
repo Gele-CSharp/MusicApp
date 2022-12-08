f24680b2751ab636776e56e061b8beb19af436c2
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-08 19:45:06 +0200

    HompageAlbumModel added

---
+++ MusicApp.Core/Models/Album/HomepageAlbumModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MusicApp.Core.Models.Album
+{
+    /// <summary>
+    /// Homepage album model
+    /// </summary>
+    public class HomepageAlbumModel
+    {
+        /// <summary>
+        /// Album identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Album title
+        /// </summary>
+        public string Title { get; set; } = null!;
+
+        /// <summary>
+        /// Album cover image url
+        /// </summary>
+        public string ImageUrl { get; set; } = null!;
+    }
+}
