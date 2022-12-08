0a1cdd69604fa540ab0be0d91603af8aaca31d77
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-08 11:14:21 +0200

    AlbumGenreModel created

---
+++ MusicApp.Core/Models/Album/AlbumGenreModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MusicApp.Core.Models.Album
+{
+    /// <summary>
+    /// Model for album genre
+    /// </summary>
+    public class AlbumGenreModel
+    {
+        /// <summary>
+        /// Genre identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Genre name
+        /// </summary>
+        public string Name { get; set; } = null!;
+    }
+}
