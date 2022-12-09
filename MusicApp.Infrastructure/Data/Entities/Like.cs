faec0e1a95710b5006e1786b0625b4c300c001d9
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-09 16:40:09 +0200

    Entity Like added

---
+++ MusicApp.Infrastructure/Data/Entities/Like.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicApp.Infrastructure.Data.Entities
+{
+    public class Like
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int AlbumId { get; set; }
+    }
+}
