394125ef842642d98801fb80eac8b3d1c9c230c3
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-10 17:40:44 +0200

    Statistics models added

---
+++ MusicApp.Core/Models/Statistics/StatisticsAlbumModel.cs
@@ -0,0 +1,49 @@
+﻿using MusicApp.Core.Models.Album;
+using MusicApp.Infrastructure.Data.Entities;
+
+namespace MusicApp.Core.Models.Statistics
+{
+    public class StatisticsAlbumModel : IAlbumModel
+    {
+        /// <summary>
+        /// Album identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Album title
+        /// </summary>
+        public string Title { get; set; } = null!;
+
+        /// <summary>
+        /// Performer
+        /// </summary>
+        public string Artist { get; set; } = null!;
+
+        /// <summary>
+        /// Album information
+        /// </summary>
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Album cover image
+        /// </summary>
+        public string ImageUrl { get; set; } = null!;
+
+        /// <summary>
+        /// Music genre
+        /// </summary>
+        public Genre Genre { get; set; } = null!;
+
+        /// <summary>
+        /// Album release year
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// Number of likes for current album 
+        /// </summary>
+        public int Likes { get; set; } = 0;
+    }
+}
+
---
+++ MusicApp.Core/Models/Statistics/StatisticsModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MusicApp.Core.Models.Statistics
+{
+    /// <summary>
+    /// Statistics model
+    /// </summary>
+    public class StatisticsModel
+    {
+        /// <summary>
+        /// Number of albums
+        /// </summary>
+        public int Albums { get; init; }
+
+        /// <summary>
+        /// Number of artists
+        /// </summary>
+        public int Artists { get; init; }
+
+        /// <summary>
+        /// Number of users
+        /// </summary>
+        public int Users { get; init; }
+    }
+}
