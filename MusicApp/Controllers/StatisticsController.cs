3ed3f813b286a721bfaf9720df340bcd4428771d
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-10 17:39:50 +0200

    StatisticsController added

---
+++ MusicApp/Controllers/StatisticsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApp.Core.Contracts;
+
+namespace MusicApp.Controllers
+{
+    public class StatisticsController : BaseController
+    {
+        private readonly IStatisticsService statisticsService;
+
+        public StatisticsController(IStatisticsService _statisticsService)
+        {
+            statisticsService = _statisticsService;
+        }
+
+        public async Task<IActionResult> Top3()
+        {
+            var model = await statisticsService.Top3();
+            return View(model);
+        }
+    }
+}
