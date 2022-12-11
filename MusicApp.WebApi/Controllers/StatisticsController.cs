3d3020c249c06cdbf29d83e0acabf2b5ad8b62fc
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-11 14:14:20 +0200

    StatisticsController added

---
+++ MusicApp.WebApi/Controllers/StatisticsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApp.Core.Contracts;
+using MusicApp.Core.Models.Statistics;
+
+namespace MusicApp.WebApi.Controllers
+{
+    [Route("api/statistics")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly IStatisticsService statisticsService;
+
+        public StatisticsController(IStatisticsService _statisticsService)
+        {
+            statisticsService = _statisticsService;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200, Type = typeof(StatisticsModel))]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> Statistics()
+        {
+            var model = await statisticsService.Statistics();
+            return Ok(model);
+        }
+    }
+}
