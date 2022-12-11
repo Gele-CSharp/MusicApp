a874be4e5840143ca9152ee08264add08c900447
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-11 14:13:57 +0200

    Added

---
+++ MusicApp.WebApi/Extensions/MusicAppServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using HouseRentingSystem.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using MusicApp.Core.Contracts;
+using MusicApp.Core.Services;
+using MusicApp.Data;
+using MusicApp.Infrastructure.Common;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class MusicAppServiceCollectionExtension
+{
+    public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IStatisticsService, StatisticsService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddMusicAppDbContext(this IServiceCollection services, IConfiguration config)
+    {
+        var connectionString = config.GetConnectionString("DefaultConnection");
+        services.AddDbContext<MusicAppDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        services.AddScoped<IRepository, Repository>();
+        
+        return services;
+    }
+}
