b087b39093a96dc33545355ca7b871dfef1d7dd9
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-11 20:39:25 +0200

    Controllers added

---
+++ MusicApp/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/{id?}")]
+    [Authorize(Roles = "Administrator")]
+    public class BaseController : Controller
+    {
+    }
+}
