945a724e2f77e0c713fb0d4ec27f8bfa302e4775
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-11 14:16:08 +0200

    Statistics method added

--- MusicApp.Core/Contracts/IStatisticsService.cs
+++ MusicApp.Core/Contracts/IStatisticsService.cs
@@ -5,5 +5,7 @@ namespace MusicApp.Core.Contracts
     public interface IStatisticsService
     {
         Task<IEnumerable<StatisticsAlbumModel>> Top3();
+
+        Task<StatisticsModel> Statistics();
     }
 }
