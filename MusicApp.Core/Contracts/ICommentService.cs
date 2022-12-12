912ba7e6e440f97b2b543d0275f03c0d9f957670
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-13 00:10:20 +0200

    Added

---
+++ MusicApp.Core/Contracts/ICommentService.cs
@@ -0,0 +1,18 @@
+﻿using MusicApp.Core.Models.Comments;
+using MusicApp.Infrastructure.Data.Entities;
+
+namespace MusicApp.Core.Contracts
+{
+    public interface ICommentService
+    {
+        Task<ICollection<Comment>> GetComments(int albumId);
+
+        Task AddComment(int albumId, string userId, Comment comment);
+
+        Task<Comment> GetComment(int albumId, int commentId);
+
+        Task<EditCommentModel> GetCommentToEdit(int albumId, int commentId);
+
+        Task Edit(EditCommentModel model);
+    }
+}
