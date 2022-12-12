50e44842bef4d79286bca8c47f9ed6a6b205c82f
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-13 00:10:09 +0200

    Added

---
+++ MusicApp.Core/Models/Comments/EditCommentModel.cs
@@ -0,0 +1,15 @@
+﻿using MusicApp.Infrastructure.Data.Entities;
+
+namespace MusicApp.Core.Models.Comments
+{
+    public class EditCommentModel
+    {
+        public int Id { get; set; }
+
+        public int AlbumId { get; set; }
+
+        public string Content { get; set; } = null!;
+
+        public User User { get; set; } = null!;  
+    }
+}
