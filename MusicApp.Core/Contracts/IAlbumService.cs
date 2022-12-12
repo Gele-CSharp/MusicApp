93094a34e6a5e7d092dd6cb40cdfc7a7ac360a42
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-12 14:20:14 +0200

    Method removed

--- MusicApp.Core/Contracts/IAlbumService.cs
+++ MusicApp.Core/Contracts/IAlbumService.cs
@@ -9,6 +9,8 @@ namespace MusicApp.Core.Contracts
 
         Task<AllAlbumsModel> GetAllAlbums(string? genre = null, string? searchTerm = null, AlbumsSorting sorting = AlbumsSorting.Newest, int currentPage = 1, int albumsPerPage = 1 );
 
+        Task<AllAlbumsModel> GetAllAlbums(string? genre = null, string? searchTerm = null, AlbumsSorting sorting = AlbumsSorting.Newest);
+
         Task<IEnumerable<AlbumModel>> GetAllUserAlbums(string userId);
 
         Task<IEnumerable<Genre>> GetGenres();
