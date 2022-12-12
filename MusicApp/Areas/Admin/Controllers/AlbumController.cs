bd0032d7c1f755efcdfd798006cd69201ff1880a
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-13 00:14:56 +0200

    Actions added

--- MusicApp/Areas/Admin/Controllers/AlbumController.cs
+++ MusicApp/Areas/Admin/Controllers/AlbumController.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MusicApp.Core.Contracts;
 using MusicApp.Core.Models.Album;
+using MusicApp.Core.Models.Comments;
 
 namespace MusicApp.Areas.Admin.Controllers
 {
     public class AlbumController : BaseController
     {
         private readonly IAlbumService albumService;
+        private readonly ICommentService commentService;
 
-        public AlbumController(IAlbumService _albumService)
+        public AlbumController(IAlbumService _albumService,
+                               ICommentService _commentService)
         {
             albumService = _albumService;
+            commentService = _commentService;
         }
 
         public async Task<IActionResult> All([FromQuery] AllAlbumsModel query)
@@ -30,5 +34,51 @@ namespace MusicApp.Areas.Admin.Controllers
 
             return View(model);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Comment(CommentModel model, int albumId)
+        {
+            var userId = User.Id();
+
+            if (model.Comment != null)
+            {
+                await commentService.AddComment(albumId, userId, model.Comment);
+            }
+
+            return RedirectToAction(nameof(Details), new { albumId });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await albumService.GetAlbumDetailsToEdit(id);
+            model.Genres = await albumService.GetGenreModels();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AddAlbumModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                model.Genres = await albumService.GetGenreModels();
+                return View(model);
+            }
+
+            var userId = User.Id();
+            var albumId = model.Id;
+
+            await albumService.Edit(albumId, userId, model);
+
+            return RedirectToAction(nameof(Details), new { albumId });
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var userId = User.Id();
+            await albumService.Delete(id, userId);
+            return RedirectToAction(nameof(All));
+        }
     }
 }
+
