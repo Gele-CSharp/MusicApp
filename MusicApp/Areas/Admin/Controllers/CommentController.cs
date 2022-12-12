007c05ef639df8912936e08636d87983e4a24511
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-13 00:15:14 +0200

    Added

---
+++ MusicApp/Areas/Admin/Controllers/CommentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApp.Core.Contracts;
+using MusicApp.Core.Models.Comments;
+
+namespace MusicApp.Areas.Admin.Controllers
+{
+    public class CommentController : BaseController
+    {
+        private readonly ICommentService commentService;
+
+        public CommentController(ICommentService _commentService)
+        {
+            commentService = _commentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id, int albumId)
+        {
+            var model = await commentService.GetCommentToEdit(albumId, id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditCommentModel model)
+        {
+            await commentService.Edit(model);
+
+            return RedirectToAction("Details", "Album", new { area = "Admin", albumId = model.AlbumId });
+        }
+    }
+}
