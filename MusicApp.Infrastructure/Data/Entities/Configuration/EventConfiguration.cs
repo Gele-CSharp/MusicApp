a05b85b06dc831835e4288b46136760f263e6897
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-13 16:44:22 +0200

    Modified

--- MusicApp.Infrastructure/Data/Entities/Configuration/EventConfiguration.cs
+++ MusicApp.Infrastructure/Data/Entities/Configuration/EventConfiguration.cs
@@ -11,8 +11,9 @@ namespace MusicApp.Infrastructure.Data.Entities.Configuration
             {
                 Id = 1,
                 Artist = "Лили Иванова",
+                ImageUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.eventim.bg%2Fbg%2Fartist%2Flili-ivanova-2%2Fprofile.html&psig=AOvVaw3IDtRNU1-18kn24xJVnSN8&ust=1671022386647000&source=images&cd=vfe&ved=2ahUKEwj53bTP0fb7AhWRYPEDHUnCBqAQjRx6BAgAEAo",
                 Location = "Зала Армеец",
-                Date = new DateTime(2022, 12, 1)
+                Date = new DateTime(2022, 12, 4, 20, 0, 0)
             });
         }
     }
