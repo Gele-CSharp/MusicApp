eff7f993ba4f873a842b375533f8ad79b075c73a
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-13 16:49:21 +0200

    Added

---
+++ MusicApp.Core/Models/Album/AdminAreaAlbumModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MusicApp.Core.Models.Album
+{
+    /// <summary>
+    /// Album model for admin area
+    /// </summary>
+    public class AdminAreaAlbumModel : AlbumModel
+    {
+        /// <summary>
+        /// Determines if album to be displayed
+        /// </summary>
+        public bool IsActive { get; set; }
+    }
+}
---
+++ MusicApp.Core/Models/Album/AdminAreaAllAlbumsModel.cs
@@ -0,0 +1,55 @@
+﻿using MusicApp.Infrastructure.Data.Entities;
+
+namespace MusicApp.Core.Models.Album
+{
+    /// <summary>
+    /// Admin area all albums model
+    /// </summary>
+    public class AdminAreaAllAlbumsModel
+    {
+        /// <summary>
+        /// Albums per page
+        /// </summary>
+        public const int AlbumsPerPage = 3;
+
+        /// <summary>
+        /// Album genre
+        /// </summary>
+        public string Genre { get; set; } = null!;
+
+        /// <summary>
+        /// Search condition
+        /// </summary>
+        public string? SearchTerm { get; set; }
+
+        /// <summary>
+        /// Sorting condition
+        /// </summary>
+        public AlbumsSorting Sorting { get; set; }
+
+        /// <summary>
+        /// Number of displayed page
+        /// </summary>
+        public int CurrentPage { get; set; } = 1;
+
+        /// <summary>
+        /// Number of albums
+        /// </summary>
+        public int TotalAlbumsCount { get; set; }
+
+        /// <summary>
+        /// Genres collection
+        /// </summary>
+        public IEnumerable<Genre> Genres { get; set; } = Enumerable.Empty<Genre>();
+
+        /// <summary>
+        /// Determines if activ or deleted albums to be displayed
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Albums collection
+        /// </summary>
+        public IEnumerable<AdminAreaAlbumModel> Albums { get; set; } = Enumerable.Empty<AdminAreaAlbumModel>();
+    }
+}
