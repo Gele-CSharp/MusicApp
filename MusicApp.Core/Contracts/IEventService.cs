371443521d6f924d91e7fc3af78c171dd85d8f9d
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-15 22:55:59 +0200

    Methods added

--- MusicApp.Core/Contracts/IEventService.cs
+++ MusicApp.Core/Contracts/IEventService.cs
@@ -7,5 +7,15 @@ namespace MusicApp.Core.Contracts
         Task AddEvent(EventModel model);
 
         IEnumerable<EventModel> AdminAllEvents();
+
+        IEnumerable<EventModel> AllEvents();
+
+        Task<EventModel> EventDetailsToEdit(int eventId);
+
+        Task Edit(EventModel model);
+
+        Task Delete(int id);
+
+        Task Restore(int id);
     }
 }
