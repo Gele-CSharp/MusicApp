0be5441d808c22ede04cc05cdd39bb6e275671d7
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-15 23:04:16 +0200

    Added

---
+++ MusicApp/Controllers/EventController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MusicApp.Core.Contracts;
+
+namespace MusicApp.Controllers
+{
+    public class EventController : BaseController
+    {
+        private readonly IEventService eventService;
+
+        public EventController(IEventService _eventService)
+        {
+            eventService = _eventService;
+        }
+
+        [AllowAnonymous]
+        public IActionResult All()
+        {
+            var model = eventService.AllEvents();
+            return View(model);
+        }
+    }
+}
