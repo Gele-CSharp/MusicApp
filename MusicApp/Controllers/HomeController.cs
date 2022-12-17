4562cd327e7ef0f20af552aec3d5408d2c88d0f1
Gele-CSharp <georgiev.angel@yahoo.com>
2022-12-17 11:40:50 +0200

    MemoryCache added

--- MusicApp/Controllers/HomeController.cs
+++ MusicApp/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using MusicApp.Core.Contracts;
+using MusicApp.Core.Models.Album;
 using MusicApp.Models;
 using System.Diagnostics;
+using static MusicApp.Infrastructure.Data.DataConstants.Album;
 
 namespace MusicApp.Controllers
 {
@@ -10,18 +13,33 @@ namespace MusicApp.Controllers
     {
         private readonly ILogger<HomeController> logger;
         private readonly IAlbumService albumService;
+        private readonly IMemoryCache cache;
 
-        public HomeController(ILogger<HomeController> _logger, IAlbumService _albumService)
+
+        public HomeController(ILogger<HomeController> _logger,
+                              IAlbumService _albumService,
+                              IMemoryCache _cache)
         {
             logger = _logger;
             albumService = _albumService;
+            cache = _cache;
         }
 
         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var model = await albumService.GetLastThreeAlbums();
+            var model = cache.Get<IEnumerable<HomepageAlbumModel>>(LastThreeAlbumsCacheKey);
+
+            if (model == null)
+            {
+                model = await albumService.GetLastThreeAlbums();
+                var cacheOptions = new MemoryCacheEntryOptions()
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(3));
+
+                cache.Set(LastThreeAlbumsCacheKey, model, cacheOptions);
+            }
+
             return View(model);
         }
 
